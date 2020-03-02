1963a0e68bc0b673b0c7862fa36ca5d16810e088
Gy <297879328@qq.com>
2020-03-02 14:32:32 +0800

    1

---
+++ src/test/java/com/xianbaovip/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xianbaovip.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
